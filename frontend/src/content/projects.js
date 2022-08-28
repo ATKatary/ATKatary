7d057d0b5cfa294c4ed2ffacf481946b43e53993
Ahmed <akatary@mit.edu>
2022-08-28 13:54:11 -0400

    Adding project 5

--- frontend/src/content/projects.js
+++ frontend/src/content/projects.js
@@ -15,19 +15,22 @@ export function Projects(props) {
   return (
     <div style={{color: "black", height: "91%"}} id="projects" className="none column align-center scroll no-scrollbar">
       {/*** Ployem ***/}
-      <ImageText image={props.images("./projects/project0.png")} style={style} name={"Ployem"} text={projects['ployem']}/>
+      <ImageText image={props.images("./projects/project0.png")} style={style} name={"Ployem"} text={projects['ployem']} onClick={() => {window.location = "https://www.ployem.com"}}/>
+
+      {/*** Ployem ***/}
+      <TextImage image={props.images("./projects/project5.png")} style={style} name={"Age Gang Inc"} text={projects['apeGang']} onClick={() => {window.location = "https://www.apeganginc.com"}}/>
 
       {/*** MIT MSA ***/}
-      <TextImage image={props.images("./projects/project1.png")} style={style} name={"MIT MSA"} text={projects['msa']} onClick={() => {window.location = "https://www.mit-msa.com"}}/>
+      <ImageText image={props.images("./projects/project1.png")} style={style} name={"MIT MSA"} text={projects['msa']} onClick={() => {window.location = "https://www.mit-msa.com"}}/>
       
       {/*** Star Front ***/}
-      <ImageText image={props.images("./projects/project2.png")} style={style} name={"StarFront"} text={projects['starFront']}/>
+      <TextImage image={props.images("./projects/project2.png")} style={style} name={"StarFront"} text={projects['starFront']} onClick={() => {window.location = "https://www.ployem.com/glab/starfront/?puzzleId=1"}}/>
 
       {/*** iLab ***/}
-      <TextImage image={props.images("./projects/project3.png")} style={style} name={"iLab"} text={projects['iLab']}/>
+      <ImageText image={props.images("./projects/project3.png")} style={style} name={"iLab"} text={projects['iLab']} onClick={() => {window.location = "https://www.ployem.com/ilab"}}/>
 
       {/*** Simba ***/}
-      <ImageText image={props.images("./projects/project4.png")} style={style} name={"Simba"} text={projects['simba']}/>
+      <TextImage image={props.images("./projects/project4.png")} style={style} name={"Simba"} text={projects['simba']}/>
     </div>
   )
 }
